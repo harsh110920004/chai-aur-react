901b3b23be04054ce4b6e031b0f140d8de8436d9
Harsh <hy7486905@gmail.com>
2025-01-20 23:58:09 +0530

    update

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+function App() {
+
+  return (
+      <>
+      <Chai/>
+      <h1> Chai aur React</h1>
+      <p> test para </p>
+      </>
+  )
+}
+
+export default App
